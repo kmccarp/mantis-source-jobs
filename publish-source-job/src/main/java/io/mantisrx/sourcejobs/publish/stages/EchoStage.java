7eea663e03d857baf0a3471e72b1a9bdf24bae22
Kevin McCarpenter™️ <kevin@moderne.io>
2023-04-04 18:32:02 +0000

    refactor: Common static analysis issues
    Co-authored-by: Moderne <team@moderne.io>

--- publish-source-job/src/main/java/io/mantisrx/sourcejobs/publish/stages/EchoStage.java
+++ publish-source-job/src/main/java/io/mantisrx/sourcejobs/publish/stages/EchoStage.java
@@ -71,12 +71,12 @@ public class EchoStage implements ScalarComputation<String, String> {
         return events
             .buffer(bufferDuration, TimeUnit.MILLISECONDS)
             .flatMapIterable(i -> i)
-            .filter((event) -> !event.isEmpty())
-            .flatMap((envelopeStr) -> {
+            .filter(event -> !event.isEmpty())
+            .flatMap(envelopeStr -> {
                 try {
                     MantisEventEnvelope envelope = mantisEventEnvelopeReader.readValue(envelopeStr);
                     return Observable.from(envelope.getEventList())
-                        .map((event) -> event.getData());
+                        .map(MantisEvent::getData);
                 } catch (IOException e) {
                     LOGGER.error(e.getMessage());
                     // Could not parse just send it along.
@@ -84,7 +84,7 @@ public class EchoStage implements ScalarComputation<String, String> {
                 }
             })
             .map(this::insertSourceJobName)
-            .onErrorResumeNext((t1) -> {
+            .onErrorResumeNext(t1 -> {
                 LOGGER.error("Exception occurred in : " + clusterName + " error is " + t1.getMessage());
                 return Observable.empty();
             });
